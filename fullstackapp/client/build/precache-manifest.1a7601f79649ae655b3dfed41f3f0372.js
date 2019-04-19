1474c48ef5887220d2d91b3c2ebd45986abb94dc
nise406 <44655381+nise406@users.noreply.github.com>
2019-04-18 19:06:54 -0600

    commit

---
+++ fullstackapp/client/build/precache-manifest.1a7601f79649ae655b3dfed41f3f0372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a21053086aea7c40fb6e4d55f8d6ba6",
+    "url": "/static/media/books-2.7a210530.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6b86587afab3bc19b9e2",
+    "url": "/static/js/main.8080fe33.chunk.js"
+  },
+  {
+    "revision": "7874690a7197f4daa250",
+    "url": "/static/js/2.77291a83.chunk.js"
+  },
+  {
+    "revision": "6b86587afab3bc19b9e2",
+    "url": "/static/css/main.40a8dcd4.chunk.css"
+  },
+  {
+    "revision": "cb5378604c03701c8c2f34a62e7c72c2",
+    "url": "/index.html"
+  }
+];
